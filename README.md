e3468ddcdc1984d316c448de50f8b4f846c6318f
Abdallah Joudeh <abdallajoudeh14@gmail.com>
2023-06-18 19:45:22 +0300

    init project, add readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Awesome Navigation Bar Menu
+
+Build a navigation bar menu using **HTML**, **CSS** & **JavaScript**.
+
+[Watch video on youtube](https://youtu.be/0RWrWHaoZx0 "Navigation Bar Menu")
+
+Video contents:
+
+* Awesome Design
+* Add `active` class when scrolling
+* Move an element when scrolling or clicking on it
+* Responsive Design for mobile devices
+
+![img](AwesomeNavigationBarMenu.png)
+
+Hey bro please don't forget to subscribe to [my channel](https://www.youtube.com/@CodingWeb3 "CodingWeb") and like the [video](https://youtu.be/0RWrWHaoZx0 "Navigation Bar Menu") 😊😊
---
+++ script.js
@@ -0,0 +1,19 @@
+const indicator = document.querySelector(".indicator"),
+	sections = document.querySelectorAll("section[id]");
+
+function activeScroll() {
+	const scrollY = window.scrollY;
+	sections.forEach((section, index) => {
+		const sectionTop = section.offsetTop - 16,
+			sectionHeight = section.offsetHeight,
+			link = document.querySelector(`.navbar__link-item a[href='#${section.id}'`);
+		if (scrollY >= sectionTop && scrollY <= sectionHeight + sectionTop) {
+			link.classList.add("active");
+			indicator.style.left = `${index * 90 + 10}px`;
+		} else {
+			link.classList.remove("active");
+		}
+	});
+}
+
+window.addEventListener("scroll", activeScroll);
